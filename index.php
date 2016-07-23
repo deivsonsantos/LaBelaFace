d49a6275030cdb796d00aaa4e67d5fa0913bf953
Deivson <dvs.santos@gmail.com>
2016-07-23 13:18:03 -0300

    merge alterações

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
 						<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> 
 					</button> 
-					<a class="navbar-brand" href="index.php"><h1><img src="images/logo.png" alt="logo"></h1></a> 
+					<a class="navbar-brand" href="index.php"><h1><img src="images/logo.png" alt="logo" class="logo-marca"></h1></a> 
 				</div> 
 				<div class="collapse navbar-collapse"> 
 					<ul class="nav navbar-nav navbar-right"> 
@@ -625,7 +625,7 @@
 						<div class="container">
 							<div class="row text-center clearfix">
 								<div class="col-sm-8 col-sm-offset-2">
-									<h2 class="title-one">Our Blog</h2>
+									<h2 class="title-one">Blog</h2>
 									<p class="blog-heading">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 								</div>
 							</div> 
@@ -641,7 +641,7 @@
 										<div class="blog-content">
 											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 										</div>
-										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-detail">Read More</a>
+										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-detail">Ler Mais</a>
 									</div>
 									<div class="modal fade" id="blog-detail" tabindex="-1" role="dialog" aria-hidden="true">
 										<div class="modal-dialog">
@@ -667,7 +667,7 @@
 										<div class="blog-content">
 											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 										</div>
-										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-two">Read More</a>
+										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-two">Ler Mais</a>
 									</div>
 									<div class="modal fade" id="blog-two" tabindex="-1" role="dialog" aria-hidden="true">
 										<div class="modal-dialog">
@@ -693,7 +693,7 @@
 										<div class="blog-content">
 											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 										</div>
-										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-three">Read More</a>
+										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-three">Ler Mais</a>
 									</div>
 									<div class="modal fade" id="blog-three" tabindex="-1" role="dialog" aria-hidden="true">
 										<div class="modal-dialog">
@@ -719,7 +719,7 @@
 										<div class="blog-content">
 											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 										</div>
-										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-four">Read More</a></div>
+										<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-four">Ler Mais</a></div>
 										<div class="modal fade" id="blog-four" tabindex="-1" role="dialog" aria-hidden="true">
 											<div class="modal-dialog">
 												<div class="modal-content">
@@ -744,7 +744,7 @@
 											<div class="blog-content">
 												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 											</div>
-											<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-six">Read More</a>
+											<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-six">Ler Mais</a>
 										</div>
 										<div class="modal fade" id="blog-six" tabindex="-1" role="dialog" aria-hidden="true">
 											<div class="modal-dialog">
@@ -771,7 +771,7 @@
 											<div class="blog-content">
 												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 											</div>
-											<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-seven">Read More</a>
+											<a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-seven">Ler Mais</a>
 										</div>
 										<div class="modal fade" id="blog-seven" tabindex="-1" role="dialog" aria-hidden="true">
 											<div class="modal-dialog">
@@ -796,7 +796,11 @@
 									<div class="col-sm-8 col-sm-offset-2">
 										<div class="contact-heading">
 											<h2 class="title-one">Contate-nos</h2>
+<<<<<<< HEAD
 											<p>Oferecemos o melhor suporte para tirar suas dúvidas.</p>
+=======
+											<p>Oferecemos o melhor suporte para tirar suas dúvidas</p>
+>>>>>>> 04198cef4461bbd77191b6da1d16278a2782b528
 										</div>
 									</div>
 								</div>
@@ -806,7 +810,11 @@
 									<div class="pattern"></div>
 									<div class="row text-center clearfix">
 										<div class="col-sm-6">
+<<<<<<< HEAD
 											<div class="contact-address"><address><p><span>Salão</span>LaBelaFace</p><strong>Av. Vereador Dário Marsíglia<br> Nº 16-A, Tabuleiro do Martins</strong><br><small>( Lorem ipsum dolor sit amet, consectetuer adipiscing elit )</small></address>
+=======
+											<div class="contact-address"><address><p><span>Salão</span>LaBelaFace</p><strong>Av. Vereador Dário Marsíglia<br>Número 16A, Tabuleiro dos Martins</strong><br><small>( Lorem ipsum dolor sit amet, consectetuer adipiscing elit )</small></address>
+>>>>>>> 04198cef4461bbd77191b6da1d16278a2782b528
 												<div class="social-icons">
 													<a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a>
 													<a href="#"><i class="fa fa-google-plus"></i></a><a href="#"><i class="fa fa-dribbble"></i></a>
@@ -821,10 +829,17 @@
 													<div class="form-group">
 														<input type="text" name="name" class="form-control name-field" required="required" placeholder="Seu Nome"></div>
 														<div class="form-group">
+<<<<<<< HEAD
 															<input type="email" name="email" class="form-control mail-field" required="required" placeholder="Seu Email">
 														</div> 
 														<div class="form-group">
 															<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Mensagem"></textarea>
+=======
+															<input type="email" name="email" class="form-control mail-field" required="required" placeholder="Seu E-mail">
+														</div> 
+														<div class="form-group">
+															<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Menssagem"></textarea>
+>>>>>>> 04198cef4461bbd77191b6da1d16278a2782b528
 														</div> 
 														<div class="form-group">
 															<button type="submit" class="btn btn-primary">Enviar</button>
@@ -840,7 +855,7 @@
 	<footer id="footer"> 
 		<div class="container"> 
 			<div class="text-center"> 
-				<p>Copyright &copy; 2014 - <a href="http://mostafiz.me/">Mostafiz</a> | All Rights Reserved</p> 
+				<p>Copyright &copy; <?php echo date('Y'); ?> - <a href="http://labelaface.com.br/">Salão Lá Bela Face</a></p> 
 			</div> 
 		</div> 
 	</footer> <!--/#footer--> 
